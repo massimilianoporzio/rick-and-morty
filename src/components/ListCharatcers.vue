72c0209816b024019495ba012a2c442ab9c02bb3
Massimiliano <massimiliano.porzio@gmail.com>
2021-09-20 21:43:08 +0200

    First commit

---
+++ src/components/ListCharatcers.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+  <div class="chracters">
+    <div class="characters__item">
+      <h2>{{ characters }}</h2>
+    </div>
+  </div>
+</section>
+</template>
+
+<script setup>
+import {onMounted,computed} from "vue";
+import {useStore} from 'vuex'
+
+  const name =  "ListCharatcers"
+  const characters = computed(()=>{
+    return store.state.charactersFiltered
+})
+  const store = useStore()
+  onMounted(()=>{
+      store.dispatch('getCharacters')
+    })
+
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,32 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    characters: [],
+    charactersFiltered: []
   },
   mutations: {
+    setCharacters(state,payload){
+      console.log('characters',payload)
+      state.characters = payload
+      console.log('characters',state.characters)
+    },
+    setCharactersFiltered(state,payload){
+      state.charactersFiltered = payload
+    }
   },
   actions: {
+    async getCharacters({commit}) {
+      await fetch('https://rickandmortyapi.com/api/character').then(async (response)=>{
+        await response.json().then((data)=>{
+          commit('setCharacters', data.results)
+          commit('setCharactersFiltered', data.results)
+        })
+
+      }).catch((error)=>{
+        console.log(error)
+      })
+
+    },
   },
   modules: {
   }
